72cb2004219ee1d4ba84f48d9dfb252c85191a43
Francesco Camarlinghi <me@minifloppy.it>
2018-02-19 18:37:05 +0100

    Fixed throbber overlay not disappearing in CC2015

--- src/core/ui/core-throbber/core-throbber.js
+++ src/core/ui/core-throbber/core-throbber.js
@@ -1,6 +1,7 @@
 ﻿
 import CoreBase from '../../ui/core-base/core-base.js';
 import CoreVisible from '../../ui/core-visible/core-visible.js';
+import CEP from '../../CEP.js';
 import './core-throbber.less';
 
 export default CoreBase.extend({
@@ -32,6 +33,30 @@ export default CoreBase.extend({
          */
         size: 150,
 
+    },
+
+    oninit: function ()
+    {
+        this._super();
+
+        // HACK: force a hierarchy refresh in CC2015. Needed to fix a bug
+        // in CEF that prevents the overlay from being hidden correctly
+        if (CEP.APIVersion.major === 6 && CEP.APIVersion.minor === 0)
+        {
+            this.observe('visible', (newValue) =>
+            {
+                if (!newValue)
+                {
+                    setTimeout(() =>
+                    {
+                        let docElem = document.documentElement, docElemNext = docElem.nextSibling;
+                        document.removeChild(docElem);
+                        document.insertBefore(docElem, docElemNext);
+                        
+                    }, 50);
+                }
+            }, { defer: true });
+        }
     },
 
 });
